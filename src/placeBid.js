f724061a37c7c6a5d53b543a83242339b85f6ba2
aditya jain <aj257453@gmail.com>
2025-01-10 15:21:40 +0530

    Initial commit

--- src/placeBid.js
+++ src/placeBid.js
@@ -89,7 +89,7 @@ const PlaceBid=()=>{
 
     const handleSubmit=(e)=>{
         e.preventDefault();
-        if(digitRef.current.value){
+        if(digitRef.current){
             setDigit((prevDigits) => [...prevDigits, digitRef.current.value]);
         }
         digit.forEach((digi)=>{
