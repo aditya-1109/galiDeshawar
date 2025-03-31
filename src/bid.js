69993ee0b83ae10f3a72ec216e6096ef2d7fa659
aditya jain <aj257453@gmail.com>
2025-03-31 22:45:20 +0530

    forst commit

--- src/bid.js
+++ src/bid.js
@@ -13,30 +13,21 @@ const Bid=()=>{
         <div className="digit-bid">
             <div className="bid-heading"><b>DIGIT BID</b> <FaPlay className="playbid-icon" /></div>
             <div className="bid-icon-container">
-                <div onClick={()=>nevigate(`/placebid/${lotteryName}/allinone`)} className="All-in-one">
-                    <FaGamepad className="gamepad-icon" />
-                    <p>ALL IN ONE</p>
-                </div>
+                
 
                 <div onClick={()=>nevigate(`/placebid/${lotteryName}/singledigit`)} className="All-in-one">
                     <FaDiceThree className="gamepad-icon" />
                     <p>SINGLE DIGIT</p>
                 </div>
 
-                <div onClick={()=>nevigate(`/placebid/${lotteryName}/oddeven`)} className="All-in-one">
-                    <FaDiceSix className="gamepad-icon" />
-                    <p>ODD-EVEN</p>
-                </div>
+                
 
                 <div onClick={()=>nevigate(`/placebid/${lotteryName}/jodidight`)} className="All-in-one">
                     <FaDice className="gamepad-icon" />
-                    <p>JODI DIGHT</p>
+                    <p>JODI DIGIT</p>
                 </div>
 
-                <div onClick={()=>nevigate(`/placebid/${lotteryName}/redbracket`)} className="All-in-one">
-                    <FaDice className="gamepad-icon" />
-                    <p>RED-BRACKET</p>
-                </div>
+                
             </div>
 
         </div>
@@ -63,7 +54,7 @@ const Bid=()=>{
 
                 <div onClick={()=>nevigate(`/placebid/${lotteryName}/choicepanna`)} className="All-in-one">
                     <FaGem className="gamepad-icon" />
-                    <p>CHOICE PANA</p>
+                    <p>All PANA</p>
                 </div>
             </div>
 
@@ -86,40 +77,9 @@ const Bid=()=>{
 
         </div>
 
-        <div className="digit-bid">
-            <div className="bid-heading"><b>ALL PATTI</b> <FaPlay className="playbid-icon" /></div>
-            <div className="bid-icon-container">
-                <div onClick={()=>nevigate(`/placebid/${lotteryName}/singlepatti`)} className="All-in-one">
-                    <p className="spade-icon">&#9824;</p>
-                    <p>SINGLE PATTI</p>
-                </div>
+       
 
-                <div onClick={()=>nevigate(`/placebid/${lotteryName}/doublepatti`)} className="All-in-one">
-                    <p className="spade-icon"><p>&#9824;</p><p style={{marginLeft: "-20px"}}>&#9824;</p></p>
-                    <p>DOUBLE PATTI</p>
-                </div>
-
-            </div>
-
-        </div>
-
-        <div className="digit-bid">
-            <div className="bid-heading"><b>FAMILY BID</b> <FaPlay className="playbid-icon" /></div>
-            <div className="bid-icon-container">
-                <div onClick={()=>nevigate(`/placebid/${lotteryName}/jodifamily`)} className="All-in-one">
-                    <FaDice className="gamepad-icon" />
-                    <p>JODI FAMILY</p>
-                </div>
-
-                <div onClick={()=>nevigate(`/placebid/${lotteryName}/panafamily`)} className="All-in-one">
-                    
-                    <p className="spade-icon">&#9824;</p>
-                    <p>PANA FAMILY</p>
-                </div>
-
-            </div>
-
-        </div>
+        
 
         <div className="digit-bid">
             <div className="bid-heading"><b>CYCLE BID</b> <FaPlay className="playbid-icon" /></div>
